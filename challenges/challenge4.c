30e86d096d1b6100b6d60a0ec2206fb0d0f57089
BASSOU <abdelhamidbassou@gmail.com>
2021-11-10 15:05:47 +0100

    fifth commit challenge4

---
+++ challenges/challenge4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    
+    float mile,Km;
+    printf("donner moi la valeur du mile :");
+    scanf("%f",&mile);
+    Km=mile/1.609;
+    printf("la valeur du Km est %f",Km);
+    return 0;
+}
