9cd5979c408a2d24aada4771165f6999f421f601
BASSOU <abdelhamidbassou@gmail.com>
2021-11-10 14:08:29 +0100

    seconde commit added challenge_1

--- challenges/challenge1.c
+++ challenges/challenge1.c
@@ -1,12 +1,12 @@
 # include <stdio.h>
 int main()
 {
-    char nom[50];
-    char prenom[99];
+    char nom[20];
+    char prenom[20];
     int age;
-    char sexe[99];
-    printf("donner votre 
-nom");
+    char sexe[20];
+    int num;
+    printf("donner votre nom");
     scanf("%s",&nom);
     printf("donner votre prenom");
     scanf("%s",&prenom);
@@ -14,9 +14,9 @@ nom");
     scanf("%d",&age);
     printf("donner votre sexe");
     scanf("%s",&sexe);
-    printf("votre nom est %s");
-    printf("votre prenom est %s");
-    printf("votre age est %d");
-    printf("votre sexe est %s");
+    printf("donner votre num");
+    scanf("%d",&num);
+    printf(" %s %s %d %s %d",nom,prenom,age,sexe,num);
+   
     return 0;
 }
