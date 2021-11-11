d12d2b0bee7be7ea2dcd559a97e2f99fbe27c6da
BASSOU <abdelhamidbassou@gmail.com>
2021-11-11 10:57:43 +0100

    challenge10.c

---
+++ challenges/challenge10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int pi=3.14;
+    float r,cir;
+    printf("donner moi la valeur dy rayon r :");
+    scanf("%f",&r);
+    cir=2*pi*r;
+    printf(" la circonférence du cercle est cir= %f",cir);
+  
+   
+
+   
+    return 0;
+}
