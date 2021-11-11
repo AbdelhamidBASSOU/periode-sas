cc4124c92196835340889411a8947a8bcdbbcb5b
BASSOU <abdelhamidbassou@gmail.com>
2021-11-11 13:55:26 +0100

    challenge13

---
+++ challenges/challenge13.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("entrez la valeur de a en decimal :");
+    scanf("%d",&a);
+    printf("a en octal=%o,en hexadecimal a=%x",a,a);
+    return 0;
+}
